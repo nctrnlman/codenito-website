c8126cbd566fda2705808016eae7a162a8bc8f09
Khaled <makkawirelang@gmail.com>
2023-09-08 11:07:54 +0700

    second footer setup

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,18 +1,56 @@
-import React from 'react'
+import React from "react";
+import Image1 from "../assets/logo-cn/logo-light.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
-    <footer id="footer" className="bg-[#151D20] text-white py-16">
-    <div className="container mx-auto">
-      <hr className="bg-[#151D20] my-0" />
+    <footer className="bg-[#151D20] text-white py-12">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center">
+          <img src={Image1} alt="Codenito Logo" className="w-32 mb-4" />
+          {/* <p className="font-semibold text-center">
+            Codenito <br />
+            Providing reliable tech since 2023
+          </p> */}
+        </div>
+        <div className="flex items-center justify-center mt-4">
+            {/* <p className="text-lg mr-4">Follow us:</p> */}
+            <a
+              href="https://www.instagram.com/codenito.id/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:text-gray-400"
+            >
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
+          </div>
+        <nav>
 
-      <div className="text-center">
-        <hr className="bg-[#151D20] py-4 my-8" />
-        <p className="text-lg">codenito © 2023. All Rights Reserved</p>
+          <div className="grid grid-flow-col gap-4 justify-center my-4">
+            <a href="#" className="text-white hover:text-gray-400">
+              Home
+            </a>
+            <a href="#" className="text-white hover:text-gray-400">
+              Services
+            </a>
+            <a href="#" className="text-white hover:text-gray-400">
+              Portfolio
+            </a>
+            <a href="#" className="text-white hover:text-gray-400">
+              Contact
+            </a>
+          </div>
+        </nav>
+
+        <div className="text-center">
+          <hr className="bg-[#151D20] my-4 py-3" />
+          
+          <p className="text-lg mt-4">Codenito © 2023. All Rights Reserved</p>
+        </div>
       </div>
-    </div>
-  </footer>
-  )
-}
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
