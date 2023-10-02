aa2a3aaa0ba0284032259a6d93cefa1b57940fab
Rhazes Devino <rhazesd@gmail.com>
2023-10-03 01:14:57 +0700

    pull

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from 'react'
+import React from "react";
 import Navbar from "./component/Navbar";
-import Footer from './component/Footer';
-// import { Route, Routes } from "react-router-dom";
+import Footer from "./component/Footer";
+import Home from "./pages/Homepage";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
-  
     <div>
-    <Navbar />
-    {/* <Routes>
-      <Route path="/" element={</>} />
-      <Route path="/service" element={</>} />
-      <Route path="/contact" element={</>} />
-     
-    </Routes> */}
-    
-    <Footer/>
-  </div>
-  )
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
