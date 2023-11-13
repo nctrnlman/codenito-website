5c8afc6cf42827b67260c708d3990f07aa1f9639
Rhazes Devino <rhazesd@gmail.com>
2023-11-13 16:45:40 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./component/Navbar";
 import Footer from "./component/Footer";
 import Home from "./pages/Homepage";
 import { Route, Routes } from "react-router-dom";
-import 'tailwindcss/tailwind.css'; 
+import "tailwindcss/tailwind.css";
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
   return (
-    <div className="min-h-screen bg-white ">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
+    <div className="min-h-screen bg-white">
+      {loading ? (
+        <div className="flex items-center justify-center h-screen">
+          <ClimbingBoxLoader color="#FFA500" loading={loading} size={30} />
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
