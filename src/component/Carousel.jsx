16ed55e0a47825a252b27a149ce1020c6e2f8bf3
Uzlah <landakuz@gmail.com>
2023-12-28 12:24:45 +0700

    COK INI BENER GA SIH?

--- src/component/Carousel.jsx
+++ src/component/Carousel.jsx
@@ -1,70 +1,75 @@
-import { motion, AnimatePresence } from "framer-motion";
-import { useRef, useEffect, useState } from "react";
-// import { images } from "../assets/carousel-logo";
+import React, { useRef, useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+
 const importAll = (r) => r.keys().map(r);
 const images = importAll(
-  require.context("../assets/carousel-logo", false, /\.(png|jpe?g|svg)$/)
+  require.context('../assets/carousel-logo', false, /\.(png|jpe?g|svg)$/)
 );
 
 function Carousel() {
   const [width, setWidth] = useState(0);
-  const [scrollX, setScrollX] = useState(0);
-  const [direction, setDirection] = useState(1); // Direction 1 is forward, -1 is reverse
+  const [scrollY, setScrollY] = useState(0);
   const carousel = useRef();
 
   useEffect(() => {
     setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, [width]);
 
-    const intervalId = setInterval(() => {
-      const newScrollX = scrollX + direction;
+  const handleScrollDirection = () => {
+    const newScrollY = window.scrollY;
+    const newDirection = newScrollY > scrollY ? 1 : -1;
 
-      if (newScrollX >= width || newScrollX <= 0) {
-        // If the end is reached, reverse the direction
-        setDirection(-direction);
-      } else {
-        setScrollX(newScrollX);
-      }
-    }, 10);
+    // Calculate new scrollY value using modulo for looping
+    setScrollY((newScrollY + width * newDirection) % width);
+  };
 
-    return () => clearInterval(intervalId);
-  }, [scrollX, width, direction]);
+  useEffect(() => {
+    window.addEventListener('scroll', handleScrollDirection);
+
+    return () => {
+      window.removeEventListener('scroll', handleScrollDirection);
+    };
+  }, [scrollY]);
 
   return (
     <div className="py-20 bg-white">
       <div className="mx-auto p-10 items-center flex justify-center">
         <h1 className="text-5xl font-bold inline border-b-4 border-grey text-black">
-          Portofolio
+          Portfolio
         </h1>
       </div>
       <motion.div
         ref={carousel}
         className="cursor-grab pt-10"
-        whileTap={{ cursor: "grabbing" }}
-        style={{ transform: `translateX(-${scrollX}px)` }}
+        whileTap={{ cursor: 'grabbing' }}
+        style={{ transform: `translateX(-${scrollY}px)` }}
       >
         <motion.div
           drag="x"
           dragConstraints={{ right: 0, left: -width }}
-          className="flex rounded-md gap-4 "
+          className="flex rounded-md gap-4"
+          style={{
+            userDrag: 'none',
+          }}
         >
-          {images.map((image) => {
-            return (
-              <motion.div
-                className="min-h-full min-w-[30rem] p-20px"
-                key={image}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-              >
+          {images.map((image) => (
+            <motion.div
+              className="min-h-full min-w-[30rem] p-30px shadow"
+              key={image}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <div className="w-full h-full">
                 <img
                   src={image}
                   alt=""
-                  className="w-full h-full pointer-events-none rounded-lg"
-                  style={{ maxWidth: "100%", maxHeight: "100%" }}
+                  className="pointer-events-none rounded-lg"
+                  style={{ width: '100%', height: '100%' }}
                 />
-              </motion.div>
-            );
-          })}
+              </div>
+            </motion.div>
+          ))}
         </motion.div>
       </motion.div>
     </div>
